f8080242406f311c8a283fa1e60ca04a3cbd6d34
stark <simone.mattolini@gmail.com>
2017-05-29 18:56:23 +0200

    runnable jar

--- jdfa-runner/src/main/java/it/unifi/ing/jdfa/runner/Main.java
+++ jdfa-runner/src/main/java/it/unifi/ing/jdfa/runner/Main.java
@@ -8,21 +8,19 @@ import it.unifi.ing.jdfa.runner.exception.DFARunnerException;
 
 public class Main {
 
-	private static final Logger LOGGER = Logger.getGlobal();
-
 	private Main() {
 	}
-	
+
 	public static void main(String[] args) {
 		if(args.length <= 0) {
 			throw new DFARunnerException("parameters needed for execution");
 		}
-		
+
 		DFAOperation op = Runner.getOperation(args[0], args);
 		DFA dfa = Runner.buildDFA(args);
 		dfa.execute(op);
-		LOGGER.info(op.asString());
-		
+
+		System.out.println(op.asString());
 		System.exit(0);
 	}
 }
